b5112c99525d1c1ef0bfeee56a40f2f41b338e98
Kenny Sanders <kennysandbum@gmail.com>
2016-12-02 12:41:27 -0500

    homework 9 - getting there

--- homework/hw9/scrabble.js
+++ homework/hw9/scrabble.js
@@ -1,3 +1,4 @@
+$(function(){
 const w = "Word";
 const l = "Letter";
 const boardVals = [
@@ -22,42 +23,155 @@ const boardVals = [
 
 
 
-var tileDist = [9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1];
+const tileDist = [9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1];
 
+var b;
+var rack;
 
-$(function(){
 	
 
-	var b = Board("container");
+	b = new Board("board");
 	var dist = tileDist.slice();
+	$("#buttons").append($("<button>").attr("id", "move").append("Submit Move")).append($("<button>").attr("id", "resetRack").append("Reset Move"));
+	$("#move").onclick = moveFunc;
+	
+	console.log("b = ");
+	console.log(b);
+	$("#resetRack").click(resetRack);
+
+
+
+	
+
+
+
+	
 	var getTile = function(){
+		
 		var total = 0;
 		for(var i = 0; i < dist.length; i++){
 			total += dist[i];
 		}
-		var rand = Math.floor(Math.random * total);
+		//console.log("total = " + total);
+		var rand = Math.floor(Math.random() * total);
+		//console.log("rand = " + rand);
 		total = 0;
 		for(var i = 0; i < dist.length; i++){
 			if(rand < total){
-				dist[i]--;
+				dist[i]-=1;
+				//console.log("i = " + i);
+				//console.log("returning: " + String.fromCharCode(65 + i));
 				return String.fromCharCode(65 + i);
+				
 			}
-			total += dist[i]
+			total += dist[i];
 		}
 		return null;
-	}
-	
+	};
+
+	rack = [];
+	//rack.push(new Tile("C"));
+	//rack.push(new Tile("D"));
+	//$("#rack").append(rack[0].img);
+	//$("#rack").append(rack[1].img);
+	rack = fillRack(rack, getTile);
+	console.log(rack);
+	updateRack(rack, "rack");
 	
 			
 
 	
 
-});
+
+
+
+
+function moveFunc(){
+
+}
+
+
+
+function tileProm(Dist){
+	return function(){
+		
+		var total = 0;
+		for(var i = 0; i < Dist.length; i++){
+			total += Dist[i];
+		}
+		//console.log("total = " + total);
+		var rand = Math.floor(Math.random() * total);
+		//console.log("rand = " + rand);
+		total = 0;
+		for(var i = 0; i < Dist.length; i++){
+			if(rand < total){
+				Dist[i]-=1;
+				//console.log("i = " + i);
+				//console.log("returning: " + String.fromCharCode(65 + i));
+				return String.fromCharCode(65 + i);
+				
+			}
+			total += Dist[i];
+		}
+		return null;
+	};
+}
+
+function resetRack(){
+	console.log(b);
+	b.setDroppable();
+	for(var i = 0; i < rack.length; i++){
+		console.log(rack[i]);
+		rack[i].img.animate({
+			top: "0px",
+			left: "0px"
+		});//removeAttr("style");
+		rack[i].img.draggable("enable");
+	}
+}
+
+function updateRack(rack, containerName){
+	var cont = $("#" + containerName);
+	cont.empty();
+	//console.log("rack.length = " + rack.length);
+	for(var i = 0; i < rack.length; i++){
+		//console.log(rack[i].img);
+		var tempDiv = $("<div>");
+		//tempDiv.droppable({drop:handleDrop});
+		tempDiv.append(rack[i].img);
+		cont.append(tempDiv);
+	}
+	return;
+}
+function fillRack(r, tileFunc){
+	//console.log("r = ");
+	//console.log(r);
+	while(r.length < 7){
+		var c = (tileFunc());
+		//console.log("c = "  + c);
+		if(c == null){
+			return r;
+		}
+		//console.log("r = ");
+		//console.log(r);
+		r.push(new Tile(c));
+	}
+	return r.slice();
+}
 
 function Tile(c){
 	this.c = c;
 	this.img = $("<img>");
 	this.img.attr("src", "tiles/Scrabble_Tile_" + c + ".jpg");
+	this.img.draggable({
+		containment:"#container",
+		start: movedTile,
+		//stop: returnTile,
+		revert:true
+	});
+	this.img.addClass("tile");
+	this.location = null;
+	return this;
 }
 
 
@@ -69,15 +183,20 @@ function Hand(containerName){
 function Board(containerName){
 	this.cont = $("#" + containerName);
 	this.letters = [];
+	this.firstMove = true;
 	for(var i = 0; i < boardVals.length; i++){
 		var templ = [];
 		var tempe = $("<tr>");
 		//console.log(boardVals);
 		for(var j = 0; j < boardVals[i].length; j++){
-
-			var tempd = $("<td>");
+			var td = $("<td>");
+			var tempd = $("<div>");
 			tempd.attr("id", i + "," + j);
 			tempd.addClass("square");
+			tempd.droppable({
+				drop:handleDrop,
+				out: pickedUp
+			});
 			if(boardVals[i][j] == null){
 				tempd.addClass("empty");
 			}
@@ -100,7 +219,9 @@ function Board(containerName){
 					else{
 						var star = $("<img>");
 						star.attr("src", "star.png");
-						
+						star.css("height", "40px");
+						//console.log(tempd);
+						tempd.empty();
 						tempd.append(star);
 					}
 				}
@@ -109,16 +230,96 @@ function Board(containerName){
 					tempd.append("2x Letter");
 				}
 			}
-			tempe.append(tempd);
+			td.append(tempd);
+			tempe.append(td);
 			templ.push(null);
 		}
 		this.cont.append(tempe);
+		this.letters.push(templ);
 	}
-	$("#7,7").empty();
-	var star = $("<img>");
-	star.attr("src", "star.png");
-	//console.log(star);
-	console.log($("#7,7"));
-	$("#7,7").append(star);
-	this.cont.children()[7].children()[7].append(star);
 }
+Board.prototype.placeTile = function (tile, pos){
+	if(this.letters[pos[0]][pos[1]] != null){
+		return false;
+	}
+	else{
+		this.letters[pos[0]][pos[1]] = tile;
+		$(tile).draggable.draggable('disable');
+	}
+}
+
+	Board.prototype.setDroppable = function(){
+		var squares = this.cont.find(".square");
+		console.log(squares);
+		for(var i = 0; i < squares.length; i++){
+			var id = String($(squares[i]).attr("id"));
+			console.log( id);
+			var x = id.slice(0, id.indexOf(","));
+			var y = id.slice(id.indexOf(","), id.length);
+			if(this.letters[x][y] == null){
+				$(squares[i]).droppable('enable');
+			}
+			else{
+				$(squares[i]).droppable('disable');
+			}
+		}
+		/*
+	for(var i = 0; i < this.letters.length; i++){
+			if(this.letters[i][j] == null){
+				console.log("#" + i.toString() + "," + j.toString());
+				$("" + i.toString() + "," + j.toString()).droppable('enable');
+				$("" + i.toString() + "," + j.toString()).css("background-color", "green")
+				/*
+				$("#" + i + "," + j).droppable({drop:handleDrop,
+								out: pickedUp });
+				.droppable( "option", "disabled", false );
+				console.log("got here, i = " + i + ", j = " + j);
+				console.log($("#" + i + "," + j));
+			}
+			else{	
+				$("#" + i + "," + j).droppable("disable");
+			}
+		}
+	}*/
+		
+	}
+
+function handleDrop(event, ui){
+	
+	ui.draggable.draggable({revert:false});
+	ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+	ui.draggable.draggable("disable");
+	//console.log(ui.draggable.location);
+	ui.draggable.location = $(this).attr("id");
+	//console.log(ui.draggable.location);
+	$(this).droppable('disable');
+	//$(this).droppable('disable');*/
+	/*if($(this).find(".ui-draggable").length == 0){
+		
+		$(this).parent().append($(ui.draggable));
+
+		console.log($(this));
+		$(ui).parent().remove($(ui.draggable));
+		$(this).css("position", "absolute");
+	}
+	else{
+		ui.draggable.position(oldPos);
+	}*/
+}
+var oldPos = null;
+function pickedUp(event, ui){
+	//console.log(event.target);
+	//$(event.target).css("position", "fixed");
+	
+}
+function movedTile(event, ui){
+	$(this).draggable({revert:true});
+	oldPos = $(this).position();
+	//console.log("got new position");
+}
+
+//function returnTile(event, ui){
+//	$(this).position(oldPos);
+//	oldPos = null;
+//}
+});
